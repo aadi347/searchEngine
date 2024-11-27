44c399f440efafc69cb874f4cc83163bc5afdbe6
Aditya Kumar <aadi678a@gmail.com>
2024-11-27 22:59:57 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import axios from 'axios'
+
+const app = express()
+const port = 3000;
+
+
+
+app.listen(port, () =>{
+    console.log(`server is up and listen on port ${port}`)
+})
